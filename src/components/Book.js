6c3377069ca04c262007843d976058d44e3e4119
Amanda Smith <ajs.amandasmith@gmail.com>
2022-09-05 16:20:31 -0500

    Updates code to use sinatra get request and removes redundant fetch request

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,23 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 function Book({ book }) {
-  console.log('book', book)
-  const [authorName, setAuthorName] = useState('');
-
-  useEffect(() => {
-    fetch(`http://localhost:9292/authors/${book.author_id}`)
-      .then(r => r.json())
-      .then(author => {
-        setAuthorName(author.name);
-      })
-  }, [])
 
   return (
     <div>
       <h1>{book.title}</h1>
       <h3>{book.publisher}</h3>
       <h4>{book.genre}</h4>
-      <h5>{authorName}</h5>
+      <h5>{book.author.name}</h5>
+      <p>{book.review}</p>
     </div>
   )
 }
